601cba29db9b59445d8b6b727f2dbcfd516ce469
sudama011 <sdhakad011@gmail.com>
2025-08-20 16:46:25 +0530

    refactor: update users feature to use generic pagination
    - Replace custom PaginatedUsers with generic PaginatedResponse<UserModel>
    - Update users datasource and repository to use shared models
    - Maintain consistent pagination structure across all features
    - Remove code duplication while preserving functionality

--- lib/features/users/data/repositories/users_repository.dart
+++ lib/features/users/data/repositories/users_repository.dart
@@ -1,9 +1,12 @@
 // lib/features/users/data/repositories/users_repository.dart
 import 'package:surabhi/features/users/data/datasources/users_remote_datasource.dart';
+import 'package:surabhi/core/data/models/paginated_response.dart';
+import 'package:surabhi/core/data/models/user_model.dart';
 
 class UsersRepository {
   final UsersRemoteDataSource remote;
   UsersRepository(this.remote);
 
-  Future<PaginatedUsers> getUsers({int page = 1, int size = 20}) => remote.getUsers(page: page, size: size);
+  Future<PaginatedResponse<UserModel>> getUsers({int page = 1, int size = 20}) =>
+      remote.getUsers(page: page, size: size);
 }
